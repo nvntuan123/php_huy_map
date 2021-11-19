e22e7fa1b9ef146a2b70f72b13b65415cfbce25d
Nhat Tuan <nvntuan123@gmail.com>
2021-11-19 22:04:30 +0700

    function in php

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
+function tinhTong($a, $b)
+{
+    return $a + $b;
+}
+
+echo tinhTong(1, 2) . '<br>';
+
 $iLength = 5;
 
-echo "Length = " . $iLength;
+echo "Length = " . $iLength . '<br>';
 
 echo "For:" . "<br>";
 for ($i = 0; $i < $iLength; $i++) { 
@@ -29,7 +36,15 @@ int iLength = 5;
 for (khoi tao; dieu kien lap; buoc lap)
 C: printf("Ahihi");
 C++: cout << "Ahihi";
+void ten_thu_tuc(danh sach tham so)
+{
 
+}
+
+<kieu du lieu tra ve> ten_ham(danh sach tham so)
+{
+
+}
 
 C#: int iLength = 5;
 
@@ -38,6 +53,7 @@ PHP:
 $iLength = 5;
 for(khoi tao; dieu kien lap; buoc lap)
 echo "Ahihi" hoac echo 'Ahihi'
+function ten_ham(danh sach tham so)
 
 Python: iLength = 5
 */
