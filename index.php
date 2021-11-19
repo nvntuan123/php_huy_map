84c06d78591ece08c7a87fe10da8c2e0a5115ee8
Nhat Tuan <nvntuan123@gmail.com>
2021-11-19 22:00:56 +0700

    update for loop

--- index.php
+++ index.php
@@ -3,19 +3,22 @@ $iLength = 5;
 
 echo "Length = " . $iLength;
 
+echo "For:" . "<br>";
 for ($i = 0; $i < $iLength; $i++) { 
-    echo 'i = ' . $i;
+    echo 'i = ' . $i . '<br>';
 }
 
+echo "While:" . "<br>";
 $a = 1;
 while ($a <= 10) {
-    echo 'a = ' . $a;
+    echo 'a = ' . $a . '<br>';
     $a++;
 }
 
+echo "Do While:" . "<br>";
 $b = 15;
 do {
-    echo 'b = ' . $b;
+    echo 'b = ' . $b . '<br>';
     $b++;
 } while ($b <= 20);
 
