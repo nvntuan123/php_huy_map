b05891ca930f84ce5d1ad23407e2f929da59a830
Nhat Tuan <nvntuan123@gmail.com>
2021-11-19 21:51:15 +0700

    init project php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+$iLength = 5;
+
+echo "Length = " . $iLength;
+
+for ($i = 0; $i < $iLength; $i++) { 
+    echo 'i = ' . $i;
+}
+
+/*
+Ngon ngu bien dich.
+C/C++:
+int iLength = 5;
+for (khoi tao; dieu kien lap; buoc lap)
+C: printf("Ahihi");
+C++: cout << "Ahihi";
+
+
+C#: int iLength = 5;
+
+Ngon ngu thong dich:
+PHP:
+$iLength = 5;
+for(khoi tao; dieu kien lap; buoc lap)
+echo "Ahihi" hoac echo 'Ahihi'
+
+Python: iLength = 5
+*/
+
